d7d83209a50cdc2c0a9db0dd62ac9565ba90fda7
VokusX <davidvoicu@rogers.com>
2018-02-04 11:43:12 -0500

    cleaned up again

--- post.py
+++
@@ -1,9 +0,0 @@
-import json
-import webapp2
-import google.appengine
-from main import Handler
-
-class GetData(Handler):
-    def post(self):
-        data = json.loads(self.request.body)
-        motionValue = data["status"][0]["value"]
